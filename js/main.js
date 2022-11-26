cbaf6cc0bf70cc8c91f2887a253969e3dffd93be
dmitriy112233 <87652199+dmitriy112233@users.noreply.github.com>
2022-11-27 00:26:35 +0200

    Added burger button

---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function () {
+    const burgerBtn = document.querySelector('.header__burger');
+    const menu = document.querySelector('.header__menu');
+    burgerBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+    })
+}());
